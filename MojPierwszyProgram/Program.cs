72d376eab22e35be313d526592b31ea5cd109317
piotrowinka2410 <piotrowinka2410@gmail.com>
2017-03-04 21:25:03 +0100

    MojPierwszyCommit

---
+++ MojPierwszyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MojPierwszyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoje imię");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Witaj {name}, miło Cię widzieć. Podaj swój ulubiony kolor");
+            var color = Console.ReadLine();
+            Console.WriteLine($"Cieszę się, że Twój ulubiony kolor to {color}" );
+            Console.ReadKey();
+
+        }
+    }
+}
